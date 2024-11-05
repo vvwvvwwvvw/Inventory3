05c04de1f0ffa06085497baf8552789cfdeabdbc
남정욱 <wjddnr3304@gmail.com>
2024-11-05 15:12:13 +0900

    Main 로직

--- Inventory3/Program.cs
+++ Inventory3/Program.cs
@@ -28,7 +28,46 @@ namespace Inventory3
         }
         static void Main(string[] args)
         {
-            
+            int[] inpt = new int[31];
+            int[] output = new int[31];
+            while (true)
+            {
+                Console.WriteLine("# 1=입고 , 2=출고 , 3=총입고량 , 4=총출고량 #");
+                Console.WriteLine("# 5=재고량 , 6=월 현황 출력 , 0=프로그램 종료 #");
+                Console.Write("원하는 작업을 입력하세요");
+
+                int command = int.Parse(Console.ReadLine());
+
+                if (command == 0)
+                {
+                    break;
+                }
+                switch (command)
+                {
+                    case 1:
+                        Console.WriteLine("** 입고처리");
+                        Process(inpt);
+                        break;
+                    case 2:
+                        Console.WriteLine("** 출고처리");
+                        Process(output);
+                        break;
+                    case 3:
+                        Console.WriteLine("** 총 입고량은");
+                        ShowSum(inpt);
+                        break;
+                    case 4:
+                        Console.WriteLine("** 총 출고량은");
+                        ShowSum(output);
+                        break;
+                    case 5:
+                        ShowCurrentStock(inpt, output);
+                        break;
+                    case 6:
+                        ShowMonthHistory(inpt, output);
+                        break;
+                }
+            }           
         }
     }
 }
