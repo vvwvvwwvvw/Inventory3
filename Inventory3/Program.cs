a15b0ef9c84a81e894d1ceb3f9fc0a0c9bc52676
남정욱 <wjddnr3304@gmail.com>
2024-11-05 14:50:13 +0900

    초기 커밋.

---
+++ Inventory3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Inventory3
+{
+     class Program
+    {
+        // 입출고 내용 저장
+        static void Process(int[] x)
+        {
+
+        }
+        // 배열 x 의 합을 출력
+        static void ShowSum(int[] x)
+        {
+
+        }
+        // 총입고 - 총출고를 계산하여 현재고를 보여줌
+        static void ShowCurrentStock(int[] input, int[] output) 
+        {
+        
+        }
+        // 한달동안 일별 입출고 내역을 보여준다
+        static void ShowMonthHistory(int[] input, int[] output)
+        {
+        
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
